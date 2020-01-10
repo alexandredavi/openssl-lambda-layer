fbad9b3aa9af2a28e3ef706a959bd4ac713bc542
Alexandre Zanelatto <alexandre.zanelatto@supero.com.br>
2020-01-10 14:29:48 -0300

    fix Docker volumes mapping

--- src/build.sh
+++ src/build.sh
@@ -4,4 +4,4 @@ rm layer/layer.zip
 
 docker image build -t openssl-layer .
 
-docker run --rm -v "$PWD"layer/:/tmp/layer openssl-layer
+docker run --rm -v "$PWD"/layer/:/tmp/layer openssl-layer
--- src/test.sh
+++ src/test.sh
@@ -2,5 +2,5 @@
 
 rm -rf layer/files && unzip layer/layer.zip -d layer/files
 
-docker run --rm -v "$PWD"test/:/var/task -v "$PWD"layer/files:/opt lambci/lambda:nodejs10.x index.handler
+docker run --rm -v "$PWD"/test/:/var/task -v "$PWD"/layer/files:/opt lambci/lambda:nodejs10.x index.handler
 
