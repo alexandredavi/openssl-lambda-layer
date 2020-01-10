2b8e877c2e508b078828a517c66e92082bd2d202
Alexandre Zanelatto <alexandre.zanelatto@supero.com.br>
2020-01-10 14:33:19 -0300

    fix layer zip path

--- src/publish.sh
+++ src/publish.sh
@@ -9,6 +9,6 @@ for region in $REGIONS; do
   aws lambda add-layer-version-permission --region $region --layer-name $LAYER_NAME \
     --statement-id sid1 --action lambda:GetLayerVersion --principal '*' \
     --version-number $(aws lambda publish-layer-version --region $region --layer-name $LAYER_NAME \
-      --zip-file fileb://layer.zip --cli-read-timeout 0 --cli-connect-timeout 0 \
+      --zip-file fileb://layer/layer.zip --cli-read-timeout 0 --cli-connect-timeout 0 \
       --description "OpenSSL binaries for Amazon Linux 2 Lambdas" --query Version --output text)
 done
