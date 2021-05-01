c2b72c2c05e1d9eb965b481ae9fd9861c7b6d00d
Ed <barp.edoardo@gmail.com>
2021-05-01 19:59:51 +0200

    Use stdout input instead of file to avoid permission errors

--- src/test/index.js
+++ src/test/index.js
@@ -1,5 +1,5 @@
 const { execSync } = require('child_process')
 
 exports.handler = async(event, context) => {
-    execSync(' openssl genrsa -out testCert.key 2048', { encoding: 'utf8', stdio: 'inherit' })
+    execSync(' openssl genrsa 2048', { encoding: 'utf8', stdio: 'inherit' })
 }
