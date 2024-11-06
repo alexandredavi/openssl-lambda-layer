d8edd66743eda292ed5f9ab87b890272333de786
Joris Vandermeersch <joris.vandermeersch@vrt.be>
2024-11-06 10:21:14 +0100

    Update scripts to use newest amazonlinux and openssl versions

---
+++ src/build-layer.sh
@@ -0,0 +1,9 @@
+cd /tmp/layer && \
+    mkdir -p bin && \
+    mkdir -p lib && \
+    cp /bin/openssl ./bin/openssl && \
+    cp /usr/lib64/libbz2.so.1 ./lib && \
+    cp /usr/lib64/libssl.so.3 ./lib && \
+    cp /usr/lib64/libcrypto.so.3 ./lib && \
+    zip -r layer.zip ./* && \
+    rm -rf lib bin
